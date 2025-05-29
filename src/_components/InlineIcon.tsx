b207966d0aa151952d2ffd9bb8b5fa56ed6e812f
Alexey Novikov <me@x3lfy.space>
2025-05-29 21:43:35 +0300

    add some cringe code

--- src/_components/InlineIcon.tsx
+++ src/_components/InlineIcon.tsx
@@ -1,7 +1,16 @@
 export interface InlineIconProps {
   src: string;
+  alt: string | undefined;
+  style: any | undefined;
 }
 
-export default function InlineIcon({ src }: InlineIconProps) {
-  return <img src={src} className="inline h-[1.4em] w-[1.4em] align-text-bottom" />;
+export default function InlineIcon({ src, alt, style }: InlineIconProps) {
+  return (
+    <img
+      src={src}
+      className="inline h-[1.4em] w-[1.4em] align-text-bottom"
+      alt={alt}
+      style={style}
+    />
+  );
 }
